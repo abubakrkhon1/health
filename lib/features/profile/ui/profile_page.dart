5e62e51117e7154cc2603e39163003323ed72216
unknown <abubakrkhon@usf.edu>
2025-05-24 18:34:42 -0400

    fix profile page for ios

--- lib/features/profile/ui/profile_page.dart
+++ lib/features/profile/ui/profile_page.dart
@@ -16,105 +16,107 @@ class ProfilePage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
-          child: Container(
-            padding: EdgeInsets.only(top: 25),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/user.png', width: 150),
-                SizedBox(height: 15),
-                Text(
-                  'Savannah Cooper, 67',
-                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(fontSize: 20, fontWeight: FontWeight.w600),
-                ),
-                SizedBox(height: 15),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    children: [
-                      CardButton(
-                        icon: Icons.person_outlined,
-                        title: 'Profile',
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ProfileSettingsPage(),
-                            ),
-                          );
-                        },
-                      ),
-                      CardButton(
-                        icon: Icons.medication,
-                        title: 'Medications',
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => MedicationPage(title:''),
-                            ),
-                          );
-                        },
-                      ),
-                      CardButton(
-                        icon: Icons.calendar_month,
-                        title: 'Appointments',
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => AppointmentsPage(title:''),
-                            ),
-                          );
-                        },
-                      ),
-                      CardButton(
-                        icon: Icons.card_membership,
-                        title: 'Medical Card',
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => MedicalCardPage(),
-                            ),
-                          );
-                        },
-                      ),
-                      CardButton(
-                        icon: Icons.health_and_safety_outlined,
-                        title: 'Insurance',
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => InsurancePage(),
-                            ),
-                          );
-                        },
-                      ),
-                      CardButton(
-                        icon: Icons.settings_outlined,
-                        title: 'Settings',
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => SettingsPage(),
-                            ),
-                          );
-                        },
-                      ),
-                      CardButton(
-                        icon: Icons.logout,
-                        title: 'Logout',
-                        onPressed: () {
-                          print('clicked on logout');
-                        },
-                      ),
-                    ],
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(top: 25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/user.png', width: 150),
+                  SizedBox(height: 15),
+                  Text(
+                    'Savannah Cooper, 67',
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(fontSize: 20, fontWeight: FontWeight.w600),
                   ),
-                ),
-              ],
+                  SizedBox(height: 15),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        CardButton(
+                          icon: Icons.person_outlined,
+                          title: 'Profile',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ProfileSettingsPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        CardButton(
+                          icon: Icons.medication,
+                          title: 'Medications',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MedicationPage(title:''),
+                              ),
+                            );
+                          },
+                        ),
+                        CardButton(
+                          icon: Icons.calendar_month,
+                          title: 'Appointments',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AppointmentsPage(title:''),
+                              ),
+                            );
+                          },
+                        ),
+                        CardButton(
+                          icon: Icons.card_membership,
+                          title: 'Medical Card',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MedicalCardPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        CardButton(
+                          icon: Icons.health_and_safety_outlined,
+                          title: 'Insurance',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => InsurancePage(),
+                              ),
+                            );
+                          },
+                        ),
+                        CardButton(
+                          icon: Icons.settings_outlined,
+                          title: 'Settings',
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SettingsPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        CardButton(
+                          icon: Icons.logout,
+                          title: 'Logout',
+                          onPressed: () {
+                            print('clicked on logout');
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
